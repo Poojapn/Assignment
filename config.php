9fb67fe69518644a0c5578203e119ad8a567592c
Poojapn <pjamp15@gmail.com>
2020-07-17 03:19:22 +0530

    added

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-$databaseHost = 'localhost';
+$databaseHost = 'mysql://be3cb8c8bd1de1:bf433341@us-cdbr-east-02.cleardb.com/heroku_6d1e981c0bed683?reconnect=true';
 $databaseName = 'mysql';
-$databaseUsername = 'root';
-$databasePassword = '';
+$databaseUsername = 'be3cb8c8bd1de1';
+$databasePassword = 'heroku_6d1e981c0bed683';
 
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
  
@@ -11,3 +11,14 @@ $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $d
 
 
 ?>
+<!-- 
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+?> -->
